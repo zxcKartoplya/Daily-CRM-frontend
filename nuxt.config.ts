d6b0e06e9d060af410806cc00a062697028341a9
Kirill <bel-kir05@mail.ru>
2025-11-21 16:35:49 +0800

    add vue-prime

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  css: ['~/assets/css/tailwind.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {}
-    }
-  }
+	compatibilityDate: '2025-07-15',
+	devtools: { enabled: true },
+	css: ['~/assets/css/tailwind.css'],
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
+	modules: ['@primevue/nuxt-module'],
+	primevue: {
+		importPT: { from: '@/passthrough/mycustompt.js' },
+	},
 })
